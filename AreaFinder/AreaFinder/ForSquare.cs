4e6252514d62fd9ec202c40e200b7ce231d50678
monarkkurosaki <monarkyadav@gmail.com>
2025-04-09 04:24:22 +0545

    try for class

---
+++ AreaFinder/AreaFinder/ForSquare.cs
@@ -0,0 +1,6 @@
+namespace AreaFinder;
+
+public class ForSquare
+{
+    
+}
--- AreaFinder/AreaFinder/Program.cs
+++ AreaFinder/AreaFinder/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to the area calculator");
+        Console.WriteLine("To find area of rectangle press r, for sqaure s and circle c");
+
+        string userSelect = Console.ReadLine();
+        if (userSelect.Equals('s'))
+        {
+            
+        }else if (userSelect.Equals('r'))
+        {
+            
+        }else if (userSelect.Equals('c'))
+        {
+            
+        }
+        else
+        {
+            Console.WriteLine("The input is invalid please try again");
+        }
     }
 }
